63ca690a77854a5cfe3b29bb0aa404e5d524e19a
koki niiyama <koki@kokinoMac-Studio.local>
2026-02-12 18:49:23 +0900

    fix: Resolve Flutter build errors
    - Add HapticFeedback import to task_row.dart
    - Update google_fonts to ^8.0.0 in pubspec.yaml

--- lib/presentation/widgets/gantt/task_row.dart
+++ lib/presentation/widgets/gantt/task_row.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../../core/theme/app_colors.dart';
 import '../../../core/constants/app_constants.dart';
